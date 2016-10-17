2aedc2ed6de25fcedcb2daaceb891be6dfcd80c5
Hack Reactor Students <accounts@hackreactor.com>
2016-10-17 16:10:05 -0700

    Add new path for event adder file

--- server.js
+++ server.js
@@ -2,13 +2,18 @@ var express = require('express');
 
 var app = express();
 
-// app.use(express.static(__dirname + '/node_modules'));
-app.use(express.static(__dirname + '/client'));
+// app.use(express.static(__dirname + '/client'));
+
 
 app.get('/', function(req, res) {
-  // console.log(__dirname);
+  res.sendFile(__dirname + '/client/eventAdder.html')
+});
+
+app.get('/calendar', function(req, res) {
+  app.use(express.static(__dirname + '/client'));  
   res.sendFile(__dirname + '/client/index.html')
-})
+});
+
 
 app.listen('3000', function() {
   console.log('server listening on port 3000');
